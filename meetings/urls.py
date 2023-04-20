dca942523e21fe70ef2a355f712e7965b3c2cb01
Abhijeet Gulhane <abhijeet.gulhane.317@gmail.com>
2023-04-20 09:29:23 -0400

    Added User Interactions with ModelForms

--- meetings/urls.py
+++ meetings/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<int:m_id>', views.detail, name="detail"),
-    path('rooms', views.rooms_list, name="rooms")
+    path('rooms', views.rooms_list, name="rooms"),
+    path('new', views.new, name="new")
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+# from django.forms import modelform_factory
 
 from .models import Meeting, Room
+from .forms import MeetingForm
 
 
 def detail(request, m_id):
@@ -11,12 +13,22 @@ def detail(request, m_id):
 
 # Please add a new page that shows a list of all room objects
 # (Just text, no link)
-
-# Create:
-#   - View
-#   - Template
-#   - URL mapping
+# Create:- View - Template - URL mapping
 
 def rooms_list(request):
     return render(request, "meetings/room_list.html",
                   {"rooms": Room.objects.all()})
+
+
+# MeetingForm = modelform_factory(Meeting, exclude=[])
+
+
+def new(request):
+    if request.method == "POST":  # form has been submitted, process data
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")  # django will send the browser the view function page and display it to the user
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
